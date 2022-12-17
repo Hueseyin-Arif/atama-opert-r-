52eb28f27641e525d4f820f8008491ab47e1b7c4
Hueseyin_Arif <yogyakartali2017@gmail.com>
2022-12-17 19:51:14 +0100

    Ekrana merhaba yazildi

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,16 @@
-console.log('Hello Wolrd');
+var text;
+var text2;
+var sonuc;
+
+sonuc = "M";
+sonuc += "e";
+//sonuc = sonuc + "e";
+sonuc += "r";
+sonuc += "h";
+sonuc += "a";
+sonuc += "b";
+sonuc += "a";
+
+
+document.write(sonuc);
+//document.getElementById('demo').innerHTML = sonuc;
